ba49ed5590b56924659838d94274baf4a7c23604
Aricly <tastyasianfood@gmail.com>
2021-10-21 14:49:12 +1100

    Trial version of SQLite statement

---
+++ Hotel119691868/Data/Migrations/20211021024013_SearchRooms.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hotel119691868.Data.Migrations
+{
+    public partial class SearchRooms : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
