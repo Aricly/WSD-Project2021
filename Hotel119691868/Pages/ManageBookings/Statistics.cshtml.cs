f27b45273f67614f1dd3e3bd5ff6325ff416f005
YelizYakar <yeliz_yakar@hotmail.com>
2021-10-21 21:01:09 +1100

    Changed booking statistics from Room to Booking

--- Hotel119691868/Pages/ManageBookings/Statistics.cshtml.cs
+++ Hotel119691868/Pages/ManageBookings/Statistics.cshtml.cs
@@ -29,7 +29,7 @@ namespace Hotel119691868.Pages.ManageBookings
 
             var statisticsGroups = _context.Customer.GroupBy(m => m.Postcode);
             StatisticsStats = await statisticsGroups.Select(g => new Statistics { Postcode = g.Key, NumberOfCustomer = g.Count() }).ToListAsync();
-            var statisticsGroup2 = _context.Room.GroupBy(m => m.ID);
+            var statisticsGroup2 = _context.Booking.GroupBy(m => m.RoomID);
             StatisticsStats2 = await statisticsGroup2.Select(g => new Statistics { RoomID = g.Key, NumberOfBooking = g.Count() }).ToListAsync();
             return Page();
         }
